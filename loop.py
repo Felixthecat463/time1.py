6c397b8341f0d455eb3107a00e84c57ce08b3eaa
Felixthecat463 <neuweilerp@gmail.com>
2021-05-31 18:56:32 +0200

    loop

---
+++ loop.py
@@ -0,0 +1,44 @@
+i = 1
+while i <=5:
+    print(i)
+    i = i + 1
+
+print("Finished!")
+
+
+
+words = ["hello", "world", "spam", "eggs"]
+for x in words:
+    print(x + "!")
+
+
+numbers = list(range(10))
+print(numbers)
+
+
+
+
+numbers = list(range(3, 8))
+print(numbers)
+
+print(range(20) == range(0, 20))
+
+
+
+numbers = list(range(5, 20, 2))
+print(numbers)
+
+
+
+for i in range(5):
+    print("hello!")
+
+
+numbers = [1]
+while True:
+    i = i + 1
+    print (i)
+
+
+
+
--- time1.py
+++ time1.py
@@ -13,8 +13,8 @@ current_time = now.strftime("%H")
 current_time=int(current_time)
 #Pause=[20,21,22,23,0,1,2,3,4,5,6,7]
 Running=[8,9,10,11,12,13,14,15,16,17,18,19]
-print(current_time in Running)
-if True:
+x=(current_time in Running)
+if x==True:
     print("Running")
 else:
     print("En pause de 20h à 8h")
